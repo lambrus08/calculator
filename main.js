b017523a2b1061eec2a866a9e474a370d236109a
ALDO LAMBRUSCHINI <adloans@gmail.com>
2015-12-28 14:19:34 -0800

    restarting

--- main.js
+++ main.js
@@ -1,43 +1,17 @@
+var numb1 = null;
+var numb2 = null;
+var operator = null;
+var numbers = [''];
+var numbers_array= 0;
+
+
 $(document).ready(function()
-{
-  $('input:button').on('click', function()
-  {
-      var inputValue = $(this).val();
-      console.log('input value:', inputValue);
-      screenDisplay(inputValue);
-  });
-
-});
-
-
-function screenDisplay(input)
-{
- var digit = $('#screenDisplay').val(input);
-
-}
-
-function add(numb1, numb2)
-{
-    return numb1 + numb2;
-}
-
-function subtract(numb1, numb2)
-{
-    return numb1 - numb2;
-}
-
-function divide(numb1, numb2)
-{
-    if (numb2 == 0)
     {
-        alert('division by 0 is a NO, NO!');
-        return 0;
+        $('.operand').on('click', function(){
+            console.log('button was clicked');
+            var digit =  $('.operand').val();
+            console.log('grabbing number value', digit);
+        })
     }
+);
 
-    return numb1 / numb2;
-}
-
-function multiply(numb1, numb2)
-{
-   return numb1 * numb2;
-}
