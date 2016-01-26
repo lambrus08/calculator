f47c744d16a33cdf383ecc695600b211d045740d
aldo lambruschini <adloans@gmail.com>
2016-01-25 21:31:08 -0800

    finished functions

--- main.js
+++ main.js
@@ -2,60 +2,208 @@ var numb1 = null;
 var numb2 = null;
 var operator = null;
 var inputStorage = [''];
+var splicedStorage = [];
 var storageArrayIndex = 0;
+var decimal = false;
 
 
-$(document).ready(function()
-    {
-        $('.operand').on('click', function()
-        {
+$(document).ready(function () {
+        $('.operand').on('click', function () {
             console.log('operand button was clicked');
             console.log(this);
-            var digit =  $(this).val();
+            var digit = $(this).val();
             console.log('grabbing number value', digit);
             addOperandsToArray(digit);
         });
 
-        $('.operator').on('click', function()
-        {
+        $('.operator').on('click', function () {
             console.log('operator was clicked');
             var op = $(this).val();
             console.log('operator is:', op);
             addOperatorsToArray(op);
+            enableDecimal();
 
         });
 
-        $('.equals').on('click', function()
-        {
+        $('.equals').on('click', function () {
             console.log('equals was clicked');
             $(this).val();
-            calculation(numb1, operator, numb2);
+            equalsCalc(numb1, operator, numb2);
+            enableDecimal()
+        });
+
+        $('#clearAll').on('click', function () {
+            console.log('clear all was clicked');
+            clearAll();
+            enableDecimal()
+        });
+
+        $('#decimal').on('click', function () {
+            console.log('decimal clicked');
+            var decimal = $(this).val();
+            console.log(decimal);
+            addDecimal();
+            addOperandsToArray(decimal);
         });
     }
 );
 
-function addOperandsToArray(number)
-{
-    console.log(number);
-    inputStorage[storageArrayIndex] += number;
+function addOperandsToArray(number) {
+    if (typeof inputStorage[storageArrayIndex] === 'number') {
+        inputStorage[storageArrayIndex] = number;
+    }
+    else {
+        console.log(number);
+        inputStorage[storageArrayIndex] += number;
+    }
+
     console.log('current array value is, operand pressed:', inputStorage);
     $('#screenDisplay').val(inputStorage[storageArrayIndex]);
-}
-
-function addOperatorsToArray(opInput)
-{
-    console.log(opInput);
-    storageArrayIndex++;
-    inputStorage[storageArrayIndex] = opInput;
-    console.log('current array value is, operator pressed:', inputStorage);
-    storageArrayIndex++;
-    inputStorage[storageArrayIndex] = '';
-    console.log('current data inside storage array:',inputStorage);
-}
-
-function calculation(numb1, operators, numb2)
-{
 
 }
 
+function addOperatorsToArray(opInput) {
+
+    if (inputStorage[inputStorage.length - 1] === '')
+    // this will execute is last item pressed was an operator
+    // or it is completely empty
+    //TODO: check if array is empty
+    {
+
+        inputStorage[inputStorage.length - 2] = opInput;
+
+
+    }
+    else {
+        if (inputStorage.length === 3) {
+            equalsCalc();
+        }
+        console.log('test one two three;', opInput);
+        storageArrayIndex++;
+        inputStorage[storageArrayIndex] = opInput;
+        console.log('current array value is, operator pressed:', inputStorage[storageArrayIndex]);
+        storageArrayIndex++;
+        inputStorage[storageArrayIndex] = '';
+        console.log('current data inside storage array:', inputStorage);
+    }
+
+}
+
+function addDecimal() {
+    if (decimal == false) {
+        $('#decimal').prop('disabled', true);
+        decimal = true;
+    }
+}
+function enableDecimal() {
+    if (decimal == true) {
+        $('#decimal').prop('disabled', false);
+        decimal = false;
+    }
+}
+
+function calculation(numb1, operator, numb2) {
+
+    switch (operator) {
+
+        case '+':
+            total = Number(numb1) + Number(numb2);
+            break;
+
+        case '-':
+            total = Number(numb1) - Number(numb2);
+            break;
+
+        case 'x':
+            total = Number(numb1) * Number(numb2);
+            break;
+
+        case '/':
+            if (numb2 === 0) {
+                return 'error';
+            } else {
+                total = Number(numb1) / Number(numb2);
+            }
+            break;
+
+
+    }
+    return total;
+
+}
+
+function equalsCalc() {
+    var numb1 = null;
+    var numb2 = null;
+    var operator = null;
+    var result = null;
+    //can we do math?
+//ie do we have 3 things in inputstorage, and the last one is a numeric string
+// if not a number, ie an empty string
+//      take the operand from inputStorage[0] and copy it to 2
+// if it isn't 3 things in input storage
+//  if it is 1 thing
+//      do I have spliceStorage?
+//          if yes, copy spliceStorage into inputStorage starting from element 2
+//          if no, we can't do math, show error
+// if not, exit with error
+
+    /*if (inputStorage.length === 3 && inputStorage[inputStorage.length - 1] === 'string')
+    {*/
+
+        for (i = 0; inputStorage.length > 1 && i < inputStorage.length; i++)
+            // loop which looks inside my input array.  If the length of array
+            // is greater than 1 and index of array is less than the array length then
+            // we increment arrays input
+        {
+            if (!isNaN(inputStorage[i])) {
+                if (numb1 === null) {
+                    numb1 = inputStorage[i];
+
+                } else {
+                    numb2 = inputStorage[i];
+                    result = calculation(numb1, operator, numb2);
+                    inputStorage[0] = result;
+                    splicedStorage = inputStorage.splice(i - 1, 2);
+                    if (typeof inputStorage[0] === 'number' && inputStorage.length === 1) {
+                        console.log('spliced storage = ', splicedStorage);
+                        inputStorage.splice(1, 2, splicedStorage[0], splicedStorage[1]);
+                        console.log('concat is complete', inputStorage);
+                    }
+
+                    console.log('equals pressed, array current status:', inputStorage);
+                    numb1 = null;
+                    numb2 = null;
+                    operator = null;
+                    storageArrayIndex = 0;
+                }
+
+            } else {
+                operator = inputStorage[i];
+            }
+        }
+
+
+   /* }else if(inputStorage.length === 3 && inputStorage[inputStorage.length -1] === ''){
+        console.log('else if statement: empty string');
+
+        inputStorage[2] = inputStorage[0];
+    }*/
+    showDisplay(result);
+
+}
+
+
+function showDisplay(currentInput) {
+    $('#screenDisplay').val(currentInput);
+}
+
+function clearAll() {
+    numb1 = null;
+    numb2 = null;
+
+    inputStorage = [''];
+    storageArrayIndex = 0;
+    $('#screenDisplay').val('0');
+}
 
